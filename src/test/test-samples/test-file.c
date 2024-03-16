e7ba8d685f140fecb79135e28e2e09fc37b30328
BastienJ <b.jubineau@gmail.com>
2024-03-16 23:40:49 +0100

    Update test-file.c with new defines and conditional statements

--- src/test/test-samples/test-file.c
+++ src/test/test-samples/test-file.c
@@ -1,5 +1,8 @@
-// #define A 1
+#define A 1
 #define B 1
+
+// Off screen test
+#ifdef A
 
 #define MAX_OK(x , y)   \
  ((x) > (y)             \
@@ -14,14 +17,60 @@
 int main() {
     int a,b;
 
+// Depth test
+#if defined A
+    int a = 1;
+
+// Middle keywords test
 #ifdef A
     int a = 1;
 #elif defined B
     int b = 2;
+#else
+    int b = 3;
 #endif
     int a = 1;
+
+#endif
 
     printf("MAX_OK: %d\n", MAX_OK(a, b));
 
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
