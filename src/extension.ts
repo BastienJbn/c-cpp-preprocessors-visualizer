21d49bdc40dab73fbb6264051ca3276afbf4cc8a
BastienJ <b.jubineau@gmail.com>
2024-03-16 22:59:22 +0100

    Fully fonctionnal search algorithm.

--- src/extension.ts
+++ src/extension.ts
@@ -1,91 +1,36 @@
 import * as vscode from 'vscode';
-
-let decorationType: vscode.TextEditorDecorationType;
+import { outlinePairUnderCursor } from "./parser";
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('[DEBUG]', 'Extension is active');
-
-    decorationType = vscode.window.createTextEditorDecorationType({
-        border: '1px solid grey',
-    });
-
+    
+    let previousEditor = vscode.window.activeTextEditor;
     let activeEditor = vscode.window.activeTextEditor;
 
-    if (activeEditor) {
-        highlightPairs(activeEditor);
-    }
-
+    // Triggered whenever the active text editor changes
     vscode.window.onDidChangeActiveTextEditor(editor => {
+        previousEditor = activeEditor;
         activeEditor = editor;
         if (activeEditor) {
-            highlightPairs(activeEditor);
+            outlinePairUnderCursor(activeEditor);
         }
     }, null, context.subscriptions);
 
+    // Triggered whenever the content of a text document changes
     vscode.workspace.onDidChangeTextDocument(event => {
         if (activeEditor && event.document === activeEditor.document) {
-            highlightPairs(activeEditor);
+            outlinePairUnderCursor(activeEditor);
         }
     }, null, context.subscriptions);
 
+    // On every change in the text editor selection
     vscode.window.onDidChangeTextEditorSelection(event => {
         if (activeEditor && event.textEditor === activeEditor) {
-            highlightPairs(activeEditor);
+            outlinePairUnderCursor(activeEditor);
         }
     }, null, context.subscriptions);
-}
-
-async function highlightPairs(editor: vscode.TextEditor) {
-    const decorations: vscode.DecorationOptions[] = [];
-    const position = editor.selection.active;
-
-    const line = editor.document.lineAt(position.line);
-    const text = line.text.trim();
-    const start = line.range.start;
-    const end = line.range.end;
-
-    // Check if the cursor is at an #ifdef or #endif directive
-    if (text.startsWith('#ifdef') || text.startsWith('#endif')) {
-        const decoration = { range: new vscode.Range(start, end) };
-        decorations.push(decoration);
-
-        // Find and highlight the corresponding keyword
-        const correspondingKeyword = findCorrespondingKeyword(editor.document, position);
-        if (correspondingKeyword) {
-            const decorationForCorrespondingKeyword = { range: correspondingKeyword };
-            decorations.push(decorationForCorrespondingKeyword);
-        }
-    }
-
-    editor.setDecorations(decorationType, decorations);
-}
-
-function findCorrespondingKeyword(document: vscode.TextDocument, position: vscode.Position): vscode.Range | undefined {
-    const line = document.lineAt(position.line);
-    const text = line.text.trim();
-
-    let targetKeyword: string;
-    if (text.startsWith('#ifdef')) {
-        targetKeyword = '#endif';
-    } else if (text.startsWith('#endif')) {
-        targetKeyword = '#ifdef';
-    } else {
-        return undefined;
-    }
-
-    for (let lineIndex = position.line + 1; lineIndex < document.lineCount; lineIndex++) {
-        const currentLine = document.lineAt(lineIndex);
-        const currentText = currentLine.text.trim();
-        if (currentText.startsWith(targetKeyword)) {
-            return currentLine.range;
-        }
-    }
-
-    return undefined;
 }
 
 export function deactivate() {
-    if (decorationType) {
-        decorationType.dispose();
-    }
+    // Nothing to do
 }
---
+++ src/parser.ts
@@ -0,0 +1,231 @@
+import * as vscode from 'vscode';
+
+let decorationType: vscode.TextEditorDecorationType = 
+vscode.window.createTextEditorDecorationType({
+    border: '1px solid grey',
+});
+
+const openingKeywords = [
+    '#if',
+    '#ifdef',
+    '#if defined',
+    '#ifndef',
+    '#if !defined',
+    '#ifeq',
+    '#ifneq'
+];
+
+const middleKeywords = [
+    '#elif',
+    '#else'
+];
+
+const closingKeywords = [
+    '#endif'
+];
+
+const enum Direction {
+    Up = -1,
+    Down = 1
+}
+
+/**
+ * @brief Highlight the matching preprocessor directives under the cursor (if any)
+ * 
+ * @param editor The text editor to highlight the pair in
+ * @returns 
+ */
+export async function outlinePairUnderCursor(editor: vscode.TextEditor) {
+    if (!editor) {
+        return; // No open text editor
+    }
+    const decorations: vscode.DecorationOptions[] = [];
+    const position = editor.selection.active;
+
+    const line = editor.document.lineAt(position.line);
+    const text = line.text.trim();
+
+    console.log('[outlinePairUnderCursor] File: ', editor.document.fileName, 
+                '  Ln:', position.line.toString().padStart(4, '0'), 
+                '  Col:', position.character.toString().padStart(4, '0'), 
+                '  Parsed: "', text, '"');
+
+    // Check if the cursor is on an opening, middle or closing preprocessor directive
+    if (text.startsWithOpeningKeyword() || text.startsWithMiddleKeyword() || text.startsWithClosingKeyword()) {
+        // Find matching keywords
+        const matchingKeywords = findMatchingKeywords(editor.document, position);
+        matchingKeywords.forEach(range => {
+            decorations.push({ range });
+        });
+
+        // If matching keywords were found, add the current line to the decorations
+        if (matchingKeywords.length > 0) {
+            decorations.push({ range: line.range });
+        }
+    }
+    else {
+        console.log('[outlinePairUnderCursor] No preprocessor directive found');
+    }
+
+    editor.setDecorations(decorationType, decorations);
+}
+
+/*
+* @brief Find the matching keywords for the given position in the document.
+* 
+* Parse the document and search for the corresponding keyword for the given position.
+* Save depth of the current keyword and search for the next keyword with the same depth.
+* 
+* @param document The document to search
+* @param position The position to search from
+* @param direction The direction to search in (0 for up, 1 for down)
+* @returns An array of ranges containing the matching keywords
+*/
+export function findMatchingKeywords(document: vscode.TextDocument, position: vscode.Position): vscode.Range[] {
+    const line = document.lineAt(position.line);
+    const text = line.text.trim();
+    const start = line.range.start;
+    const end = line.range.end;
+    
+    let ret: vscode.Range[] = [];
+
+    // If the cursor is on an opening keyword, find the closing keyword
+    if (text.startsWithOpeningKeyword()) {
+        ret = findClosingKeyword(document, position);
+    }
+
+    // If the cursor is on a closing keyword, find the opening keyword
+    if (text.startsWithClosingKeyword()) {
+        ret = findOpeningKeyword(document, position);
+    }
+
+    // If the cursor is on a middle keyword, find both the opening and closing keywords
+    if (text.startsWithMiddleKeyword()) {
+        let temp1 = findOpeningKeyword(document, position); // Find the opening keyword
+        let temp2 = findClosingKeyword(document, position); // Find the closing keyword
+        ret = temp1.concat(temp2); // Concatenate the arrays
+    }
+
+    return ret;
+}
+
+/**
+ * @brief Find the opening and middle preprocessor directive for the given position in the document (if any)
+ * 
+ * @param document The document to search
+ * @param position The position to search from
+ * @returns An array of ranges containing the opening and middle keywords (if any)
+ */
+function findOpeningKeyword(document: vscode.TextDocument, position: vscode.Position): vscode.Range[] {
+    return searchKeywords(document, position, Direction.Up);
+}
+
+/**
+ * @brief Find the closing and middle preprocessor directive for the given position in the document (if any)
+ * 
+ * @param document The document to search
+ * @param position The position to search from
+ * @returns An array of ranges containing the closing and middle keywords (if any)
+ */
+function findClosingKeyword(document: vscode.TextDocument, position: vscode.Position): vscode.Range[] {
+    return searchKeywords(document, position, Direction.Down);
+}
+
+/**
+ * @brief Search for the matching preprocessor directive for the given position in the document
+ * 
+ * Parse the document and search for the corresponding keyword for the given position.
+ * Save depth of the current keyword and search for each keyword with the same depth.
+ * Stops when a opening or closing keyword (depending on the direction) is found with the same depth.
+ * 
+ * @param document  The document to search
+ * @param position  The position to search from
+ * @param direction  The direction to search in (0 for up, 1 for down)
+ * @returns 
+ */
+function searchKeywords(document: vscode.TextDocument, position: vscode.Position, direction: Direction): vscode.Range[] {
+    const ret: vscode.Range[] = [];
+    let line = position.line + direction; // Start from the next line
+    let depth = 0;
+    let found = false;
+
+    while ( (line >= 0 && line < document.lineCount) && (!found)) {
+        // Parse the line
+        const text = document.lineAt(line).text.trim();
+
+        // Opening keyword detected
+        if ( text.startsWithOpeningKeyword() ) {
+            // If we are going up and the depth is 0, we found the last keyword
+            if ( (direction === Direction.Up) && (depth === 0) ) {
+                ret.push(new vscode.Range(line, 0, line, text.length)); // Add the range of the keyword
+                found = true; // Stop the search
+            }
+            // Otherwise, increase the depth
+            else {
+                depth += 1;
+            }
+        }
+
+        // Middle keyword detected
+        else if (text.startsWithMiddleKeyword()) {
+            // If its on the same depth, we found a matching keyword
+            if (depth === 0) {
+                ret.push(new vscode.Range(line, 0, line, text.length)); // Add the range of the keyword
+            }
+        }
+
+        // Closing keyword detected
+        else if ( text.startsWithClosingKeyword() ) {
+            // If we are going down and the depth is 0, we found the last keyword
+            if ( (direction === Direction.Down) && (depth === 0) ) {
+                ret.push(new vscode.Range(line, 0, line, text.length)); // Add the range of the keyword
+                found = true; // Stop the search
+            }
+            // Otherwise, decrease the depth
+            else {
+                depth -= 1;
+            }
+        }
+
+        // Move to the next line
+        line += direction;
+    }
+
+    return ret;
+}
+
+// Extend the String prototype to add startsWithOpeningKeyword
+declare global {
+    interface String {
+        startsWithOpeningKeyword(): boolean;
+        startsWithClosingKeyword(): boolean;
+        startsWithMiddleKeyword(): boolean;
+    }
+}
+
+/**
+ * @brief Check if the string starts with an opening preprocessor directive
+ * 
+ * @returns True if the string starts with an opening preprocessor directive, false otherwise
+ */
+String.prototype.startsWithOpeningKeyword = function() {
+    return openingKeywords.some(keyword => this.trim().startsWith(keyword));
+};
+
+/**
+ * @brief Check if the string starts with a closing preprocessor directive
+ * 
+ * @returns The keyword if the string starts with a closing preprocessor directive, false otherwise
+ */
+String.prototype.startsWithClosingKeyword = function() {
+    return closingKeywords.some(keyword => this.trim().startsWith(keyword));
+};
+
+/**
+ * @brief Check if the string starts with a middle preprocessor directive
+ * 
+ * @returns The keyword if the string starts with a middle preprocessor directive, false otherwise
+ */
+String.prototype.startsWithMiddleKeyword = function() {
+    return middleKeywords.some(keyword => this.trim().startsWith(keyword));
+};
